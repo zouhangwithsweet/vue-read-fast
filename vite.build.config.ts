519a269116fffb81e3175327b0337c3bfa780d96
zouhang <1102639563@qq.com>
2022-12-26 15:17:56 +0800

    build: update build config file

---
+++ vite.build.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      tsConfigFilePath: resolve(__dirname, './tsconfig.build.json'),
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib.ts'),
+      formats: ['iife', 'es'],
+      name: 'ReadFast',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    minify: true,
+  },
+})
--- vite.config.ts
+++ vite.config.ts
@@ -6,30 +6,15 @@ import unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts({
-    tsConfigFilePath: resolve(__dirname, './tsconfig.build.json')
-  }), unocss()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/lib.ts'),
-      formats: ['iife', 'es'],
-      name: 'ReadFast'
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-    minify: true
-  },
+  root: 'playground',
+  plugins: [
+    vue(),
+    dts({
+      tsConfigFilePath: resolve(__dirname, './tsconfig.build.json'),
+    }),
+    unocss(),
+  ],
   server: {
-    open: 'playground/index.html'
-  }
+    open: 'playground/index.html',
+  },
 })
